087d8e777c7c5ccb29eebedd60372b6151b59d5e
SHAHINSH <123875287+shahinshpbr10@users.noreply.github.com>
2024-12-20 10:43:52 +0530

    common files created

---
+++ lib/common/appcolors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+
+  static const Color bgcolor = Color(0xfFFFFF0);
+  static const Color textcolor = Color(0xf0e0e0e);
+  static const Color alerttextcolor = Color(0xfE95E47);
+
+
+
+
+ 
+
+}
