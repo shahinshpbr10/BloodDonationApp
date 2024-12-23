511302562430e4f5fcc14493f0f43603a6f12206
anas <anasputhukkolli123456@gmail.com>
2024-12-23 16:51:56 +0530

    image commit

--- lib/common/imageurl.dart
+++ lib/common/imageurl.dart
@@ -1,13 +1,50 @@
 class AppImages {
   // SVG Images
   static const String onboardingImage = 'assets/images/svg/onboardingimage.svg';
-  static const String orderFlowers = 'assets/images/svg/undraw_order-flowers_81uq.svg';
+
+  static const String about = 'assets/images/svg/about.svg';
+  static const String adduser = 'assets/images/svg/adduser.svg';
+  static const String arrow = 'assets/images/svg/arrow.svg';
+  static const String arrowleft = 'assets/images/svg/arrowleft.svg';
+  static const String arrowright = 'assets/images/svg/arrowright.svg';
+  static const String arrowtop = 'assets/images/svg/arrowtop.svg';
+  static const String bank = 'assets/images/svg/bank.svg';
+  static const String bellicon = 'assets/images/svg/bell_icon.svg';
+  static const String blood = 'assets/images/svg/blood.svg';
+  static const String bloodbankicon = 'assets/images/svg/bloodbankicon.svg';
+  static const String call = 'assets/images/svg/call.svg';
+  static const String cross = 'assets/images/svg/cross.svg';
+  static const String currentlocation = 'assets/images/svg/currentlocation.svg';
+  static const String donateblack = 'assets/images/svg/donateblack.svg';
+  static const String email = 'assets/images/svg/email.svg';
+  static const String filter = 'assets/images/svg/filter.svg';
+  static const String googlelogo = 'assets/images/svg/google_logo.svg';
+  static const String homedarkicon = 'assets/images/svg/homedarkicon.svg';
+  static const String hystory = 'assets/images/svg/hystory.svg';
+  static const String locationicon = 'assets/images/svg/location-icon.svg';
+  static const String location = 'assets/images/svg/location.svg';
+  static const String logout = 'assets/images/svg/logout.svg';
+  static const String maximize = 'assets/images/svg/maximize.svg';
+  static const String message = 'assets/images/svg/message.svg';
+  static const String moreicon = 'assets/images/svg/moreicon.svg';
+  static const String nextdonation = 'assets/images/svg/nextdonation.svg';
+  static const String nimbusstats = 'assets/images/svg/nimbus_stats.svg';
+  static const String personaldetails = 'assets/images/svg/personaldetails.svg';
+  static const String profiledark = 'assets/images/svg/profiledark.svg';
+  static const String profilegrey = 'assets/images/svg/profilegrey.svg';
+  static const String readytodonate = 'assets/images/svg/readytodonate.svg';
+  static const String requestdark = 'assets/images/svg/requestdark.svg';
+  static const String requestgrey = 'assets/images/svg/requestgrey.svg';
+  static const String requesticon = 'assets/images/svg/requesticon.svg';
+  static const String search = 'assets/images/svg/search.svg';
+  static const String shareicon = 'assets/images/svg/share_icon.svg';
+  static const String support = 'assets/images/svg/support.svg';
+  static const String toggle = 'assets/images/svg/toggle.svg';
+  static const String date = 'assets/images/svg/uiw_date.svg';
+  static const String upcoming = 'assets/images/svg/upcoming.svg';
+  static const String whitetick = 'assets/images/svg/white_tick.svg';
 
   // PNG/JPG Images
-  static const String logo = 'assets/images/logo.png';
-  static const String profilePlaceholder = 'assets/images/profile_placeholder.jpg';
-
-  // Icons
-  static const String homeIcon = 'assets/icons/home.svg';
-  static const String settingsIcon = 'assets/icons/settings.svg';
+  static const String afterdonate = 'assets/images/png/afterdonate.png';
+  static const String first = 'assets/images/png/first.png';
 }
--- lib/view/splashscreen/splashpage.dart
+++ lib/view/splashscreen/splashpage.dart
@@ -1,4 +1,5 @@
 import 'package:blood_donation_app/common/appsizes.dart';
+import 'package:blood_donation_app/common/imageurl.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -22,13 +23,20 @@ class _SplashPageState extends State<SplashPage> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
+                Image.asset(
+                  AppImages.first, // Accessing the PNG image
+                  width: 100,
+                  height: 100,
+                  fit: BoxFit.cover,
+                ),
                 SvgPicture.asset(
-                  'assets/images/svg/undraw_order-flowers_81uq.svg',
-                  width: width*0.2 , // Use 60% of screen width
-                  height: height,        // Fixed height (optional)
+                  AppImages.arrowtop, // Using the AppImages class
+                  width: width * 0.2, // Use 60% of screen width
+                  height: height, // Fixed height (optional)
                   fit: BoxFit.contain,
                   semanticsLabel: 'Onboarding Image', // Accessibility label
-                  placeholderBuilder: (context) => CircularProgressIndicator(), // Loading indicator
+                  placeholderBuilder: (context) =>
+                      CircularProgressIndicator(), // Loading indicator
                 ),
                 AppSpacing.gapSmall,
                 Padding(
