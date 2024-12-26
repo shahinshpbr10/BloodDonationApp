0dd4799ea01c003e8bc8effb8bf4bd0a023efa81
anas <anasputhukkolli123456@gmail.com>
2024-12-26 11:09:53 +0530

    color commit

--- lib/common/appcolors.dart
+++ lib/common/appcolors.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-
-  static const Color bgcolor = Color(0xfFFFFF0);
-  static const Color textcolor = Color(0xf0e0e0e);
-  static const Color alerttextcolor = Color(0xfE95E47);
-
-
-
-
- 
-
+  static const Color primary = Color(0xffffff);
+  static const Color secoundary = Color(0x0000);
+  static const Color subtextcolor = Color(0x8391A1);
+  static const Color h1textcolor = Color(0x3A3A3A);
+  static const Color orange = Color(0xFF8D00);
+  static const Color blue = Color(0x504FFE);
+  static const Color orangetext = Color(0xE95E47);
+  static const Color fillbar = Color(0x80BFB6);
+  static const Color onlinegreen = Color(0x34C759);
+  static const Color bluecontainer = Color(0x3532D7);
+  static const Color red = Color(0xFF3B30);
+  static const Color profileonline = Color(0x47E9DB);
 }
